1650cc5351dd9d017199c41c68cf7df6fd4eaaec
Hamed omari <118768898+Hamedomari@users.noreply.github.com>
2023-12-22 16:30:56 +0100

    Try8

--- src/test/java/runners/SmokeTestRunner.java
+++ src/test/java/runners/SmokeTestRunner.java
@@ -18,6 +18,6 @@ import org.junit.runner.RunWith;
         features = "./src/test/resources/features",//path of the features folder
         glue = {"stepdefinitions","hooks"},//path of the stepdefinitions folder
         dryRun = false, //generate the missing step definitions only. Do not run the existing step definitions
-        tags = " @Test12")
+        tags = " @Unit_Test")
 public class SmokeTestRunner {
 }
